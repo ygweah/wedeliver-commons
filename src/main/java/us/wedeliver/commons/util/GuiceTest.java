e29365fa06ede45487efccc2d09a63b4be2d2263
Yuhong Guo <yuhong@wedeliver.us>
2014-10-08 15:30:46 -0500

    support module list

--- src/main/java/us/wedeliver/commons/util/GuiceTest.java
+++ src/main/java/us/wedeliver/commons/util/GuiceTest.java
@@ -1,24 +1,22 @@
 package us.wedeliver.commons.util;
 
+import java.util.Properties;
 import java.util.concurrent.Callable;
 
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 
 public class GuiceTest {
-  public static final Injector INJECTOR;
+  public static Injector injector;
 
   static {
-    INJECTOR = ExceptionUtil.unchecked(new Callable<Injector>() {
+    injector = ExceptionUtil.unchecked(new Callable<Injector>() {
 
       @Override
       public Injector call() throws Exception {
-        @SuppressWarnings("unchecked")
-        Class<? extends Module> clazz = (Class<? extends Module>) Class.forName(PropertiesUtil
-            .load("/junit.properties").getProperty("junit.guice.module"));
-        Module module = clazz.newInstance();
-        return Guice.createInjector(module);
+        Properties properties = PropertiesUtil.load("/junit.properties");
+        String moduleNames = properties.getProperty("junit.guice.modules");
+        String overrideModuleNames = properties.getProperty("junit.guice.override_modules");
+        return GuiceUtil.createInjector(moduleNames, overrideModuleNames);
       }
     });
 
@@ -26,14 +24,14 @@ public class GuiceTest {
 
       @Override
       public void run() {
-        ShutdownSupport shutdownSupport = INJECTOR.getInstance(ShutdownSupport.class);
+        ShutdownSupport shutdownSupport = injector.getInstance(ShutdownSupport.class);
         shutdownSupport.shutdown();
       }
     });
   }
 
   public <T> T find(Class<T> type) {
-    return INJECTOR.getInstance(type);
+    return injector.getInstance(type);
   }
 
 }
