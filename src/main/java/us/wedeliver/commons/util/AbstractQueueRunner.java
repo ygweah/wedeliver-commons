2a9d7829a0cdfa0033bbd50892132fa55ac95992
Yuhong Guo <yuhong@wedeliver.us>
2015-01-31 08:17:21 +0800

    add base queue runner

---
+++ src/main/java/us/wedeliver/commons/util/AbstractQueueRunner.java
@@ -0,0 +1,57 @@
+package us.wedeliver.commons.util;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractQueueRunner<T> implements Runnable {
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  private BlockingQueue<T> queue;
+  private long shutdownTimeoutSeconds;
+
+  public AbstractQueueRunner(BlockingQueue<T> queue, long shutdownTimeoutSeconds) {
+    this.queue = queue;
+    this.shutdownTimeoutSeconds = shutdownTimeoutSeconds;
+  }
+
+  @Override
+  public void run() {
+    logger.debug("Start");
+    T t = null;
+    try {
+      while (!Thread.interrupted()) {
+        logger.debug("Getting from queue ...");
+        t = queue.take();
+
+        logger.debug("Processing data ...");
+        processSilently(t);
+      }
+    } catch (InterruptedException e) {
+      logger.warn("Interrupted, terminating ...", e);
+    }
+
+    try {
+      while ((t = queue.poll(shutdownTimeoutSeconds, TimeUnit.SECONDS)) != null) {
+        logger.debug("Processing data before stop");
+        processSilently(t);
+      }
+    } catch (InterruptedException e) {
+      logger.warn("Interrupted, terminate", e);
+    }
+    logger.debug("Stop");
+  }
+
+  private void processSilently(T t) {
+    try {
+      process(t);
+    } catch (Exception e) {
+      logger.warn("Processing error", e);
+    }
+  }
+
+  protected abstract void process(T t) throws Exception;
+
+}
